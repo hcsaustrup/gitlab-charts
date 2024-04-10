70a357b14920036b8d295be02a53e133fa8e9e54
Hans Christian Saustrup <hc@incrediblebits.net>
2024-04-10 12:17:48 +0200

    fix: get.sh + readme

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 A temporary place to store hotfixed Gitlab charts to facilitate upgrading an ancient installation.
 
+## Fetching original charts
+
+```
+./get.sh gitlab 2.5.5
+```
+
---
+++ get.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -eu
+
+CHART="${1:-}"
+VERSION="${2:-}"
+
+if [[ -z "$VERSION" || -z "$CHART" ]]; then
+  echo "Syntax: $0 [chart] [version]" >&2
+  exit 1
+fi
+
+helm pull gitlab/$CHART --version $VERSION
+
+FILE=$CHART-$VERSION.tgz
+if [[ ! -f $FILE ]]; then
+  echo "File not found: $FILE" >&2
+  exit 1
+fi
+
+mkdir --parent $CHART-$VERSION
+tar xvf $FILE -C $CHART-$VERSION --strip-components=1
+
+rm -rf $FILE
+
